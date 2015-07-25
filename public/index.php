0a82736a80b76fdb2225b80f06034daab40b9927
Dmitrey Kurgannikov <true.masterx2@gmail.com>
2015-07-25 17:37:39 +0300

    Add router

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: masterx2
+ * Date: 25.07.15
+ * Time: 16:26
+ */
---
+++ src/BB/Controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: masterx2
+ * Date: 25.07.15
+ * Time: 17:29
+ */
+
+namespace BB\Controllers;
+
+
+class Home
+{
+
+}
